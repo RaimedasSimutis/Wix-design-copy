db1d2cf3feed3bf2a64a5928287564e9e5c8e534
Raimedas Simutis <raimedas.simutis@gmail.com>
2019-09-19 23:14:42 +0300

    added back to top button

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ function parallax() {
     
 }
 
+function scrollToTop () {
+    window.scrollTo({top:0, left:0, behavior: 'smooth'});
+}
+
+
+
+//const scrollButton = document.querySelector('.footer-container');
+const scrollButton = document.querySelector('.back-to-top-button');
 
 window.addEventListener("scroll", function(){
 	parallax();	
 });
+
+scrollButton.addEventListener("click", function(){
+    scrollToTop();
+})
+
